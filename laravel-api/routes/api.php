32745427158331935902d71d8a9c0fff69da08a3
Marcos Vinicius Araujo da Silva <marcosviniciusjahu@gmail.com>
2023-09-21 15:57:13 -0300

    ajuste

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -15,27 +15,27 @@ use App\Http\Controller\Api;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/login',function(Request $request){
-  $credentials= $request->only(['email','password']);
+  Route::post('/login',function(Request $request){
+    $credentials= $request->only(['email','password']);
 
-  if(!$token = auth()->attempt($credentials)){
-    abort (401);
-  }
-  return response()->json([
-    'data'=> [
-        'token'=> $token,
-         'token_type' => 'bearer',
-         'expires_in' => auth()->factory()->getTTL() * 60
+    if(!$token = auth()->attempt($credentials)){
+      abort (401);
+    }
+    return response()->json([
+      'data'=> [
+          'token'=> $token,
+          'token_type' => 'bearer',
+          'expires_in' => auth()->factory()->getTTL() * 60
 
-    ]
-    ]);
-});
-Route::get('/products', [ProdutoController::class, 'index'])->middleware('auth');
-Route::post('/products', [ProdutoController::class, 'store'])->middleware('auth');
-Route::get('/products/{id}', [ProdutoController::class, 'show'])->middleware('auth');
-Route::put('/products/{id}/update', [ProdutoController::class, 'update'])->middleware('auth');
-Route::delete('/products/{id}/delete', [ProdutoController::class, 'destroy'])->middleware('auth');
-Route::middleware('api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+      ]
+      ]);
+  });
+  Route::get('/products', [ProdutoController::class, 'index'])->middleware('auth');
+  Route::post('/products', [ProdutoController::class, 'store'])->middleware('auth');
+  Route::get('/products/{id}', [ProdutoController::class, 'show'])->middleware('auth');
+  Route::put('/products/{id}/update', [ProdutoController::class, 'update'])->middleware('auth');
+  Route::delete('/products/{id}/delete', [ProdutoController::class, 'destroy'])->middleware('auth');
+  Route::middleware('api')->get('/user', function (Request $request) {
+      return $request->user();
+  });
 Route::post('/create-user',[UserController::class, 'createUser']);
